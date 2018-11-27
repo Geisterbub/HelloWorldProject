8f3126828ed503b9e9120d6eaefc8bdfbd3b90a1
Geisterbub <45207691+Geisterbub@users.noreply.github.com>
2018-11-27 17:50:45 +0100

    Update HelloWorld.java

--- HelloWorldProjekt/src/HelloWorld.java
+++ HelloWorldProjekt/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo WIN");
-// das ist ein kommentar, kein code der ausgeführt wird.
+		System.out.println("Hallo Felix Doerflinger");
+// das ist ein kommentar, kein code der ausgefÃ¼hrt wird.
 // das ist ein einziliger kommentar
 		/*
 		 * Mehrzeiligen Kommentar Schreiben
