e51e44d3cd72a0b24b35984953cd8f18a6122d18
felix.doerflinger <felix.doerflinger@KK-GYM-EDV1-09.gym.kk.local>
2018-11-27 17:39:55 +0100

    kommentare hinzugefügt

--- HelloWorldProjekt/src/HelloWorld.java
+++ HelloWorldProjekt/src/HelloWorld.java
@@ -3,7 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hallo WIN");
+		System.out.println("Hallo WIN");
+// das ist ein kommentar, kein code der ausgeführt wird.
+// das ist ein einziliger kommentar
+		/*
+		 * Mehrzeiligen Kommentar Schreiben
+		 */
 	}
 
 }
