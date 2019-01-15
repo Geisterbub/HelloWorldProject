8015b589b9bcdc6a8d32b133b187bddaabe972bd
felix.doerflinger <felix.doerflinger@KK-GYM-EDV1-09.gym.kk.local>
2019-01-15 18:05:10 +0100

    Schleife (Kleines 1x1)

---
+++ HelloWorldProjekt/src/basics/Kleine_1x1.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Kleine_1x1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+		for (int i = 1; i < 11; i++) {
+			System.out.println();
+			for(int x = 0; x < 11; x++ ) {
+
+				System.out.println(i*x);
+
+			}
+		}
+	}
+	}
+
