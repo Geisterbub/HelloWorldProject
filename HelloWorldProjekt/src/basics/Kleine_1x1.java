5213421ff03c8899251331e643158e616502579f
felix.doerflinger <felix.doerflinger@KK-GYM-EDV1-09.gym.kk.local>
2019-01-22 16:31:25 +0100

    1*1

--- HelloWorldProjekt/src/basics/Kleine_1x1.java
+++ HelloWorldProjekt/src/basics/Kleine_1x1.java
@@ -8,12 +8,14 @@ public class Kleine_1x1 {
 
 
 		for (int i = 1; i < 11; i++) {
-			System.out.println();
+			System.out.print((0+i)+"er");
 			for(int x = 0; x < 11; x++ ) {
 
-				System.out.println(i*x);
+				System.out.print("\t" + i*x);
+			
 
 			}
+			System.out.println();
 		}
 	}
 	}
