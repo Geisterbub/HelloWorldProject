44f21e254ec3254a738dcbd87964b4a48fb160c5
Felix <Felix@10.0.0.23>
2019-02-25 19:33:30 +0100

    Bubblesort

---
+++ HelloWorldProjekt/src/basics/Schleife.java
@@ -0,0 +1,38 @@
+package basics;
+
+public class Schleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				// FOR-Schleife
+
+				// 1. variable dekleration & zuweisung
+
+				// 2. die bedinung
+
+				// 3. increment / decrement
+
+				for (int i = 0; i < 10; i=i+1) {
+
+					System.out.println("Zahl: " + i);
+
+				}
+
+				//
+
+				int j = 0;
+
+				for (; j < 100;) {
+
+					System.out.println(j);
+
+					j = j+1;
+
+				}
+
+			}
+
+		
+	}
+
+
---
+++ HelloWorldProjekt/src/basics/Schleife03.java
@@ -0,0 +1,28 @@
+package basics;
+
+public class Schleife03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// while...
+
+				int zahl1 = 120;
+
+				int zahl2 = 150;
+
+				while (zahl1 % zahl2 != 0) {
+
+					zahl2 = zahl2 + 1;
+
+				}
+
+				System.out.println(zahl2);
+
+
+
+			}
+
+
+	}
+
+
