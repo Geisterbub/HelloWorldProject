b6b2f9b6cdf642dc2f5cbbfa434447f68ff87f61
felix.doerflinger <felix.doerflinger@KK-GYM-EDV1-09.gym.kk.local>
2019-03-12 18:05:58 +0100

    Qicksort

---
+++ HelloWorldProjekt/src/sortieren/QuickSort.java
@@ -0,0 +1,27 @@
+package sortieren;
+
+public class QuickSort {
+
+	static int[] zahlen = {4,2,5,9,6,10,9,7};
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for (int n = zahlen.length; n > 0; n++) { 
+
+			System.out.println(zahlen[n]); 
+	}
+		for (int links = 0; links < zahlen.length; links++)
+			for (int rechts = n - 1; rechts < zahlen.length; rechts++); 
+	if (links < rechts) {
+		int teiler = teile (links ; rechts)
+				
+			
+	
+	
+	
+
+}
+}
