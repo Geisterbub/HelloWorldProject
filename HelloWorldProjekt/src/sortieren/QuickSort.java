fde8552883d062f3c35fc975b5454980903fbdd5
felix.doerflinger <felix.doerflinger@KK-GYM-EDV1-09.gym.kk.local>
2019-03-19 18:04:58 +0100

    Quicksort

--- HelloWorldProjekt/src/sortieren/QuickSort.java
+++ HelloWorldProjekt/src/sortieren/QuickSort.java
@@ -2,26 +2,69 @@ package sortieren;
 
 public class QuickSort {
 
-	static int[] zahlen = {4,2,5,9,6,10,9,7};
-	
-	
+	static int[] zahlen = { 4, 2, 5, 9, 6, 10, 9, 7 };
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		for (int n = zahlen.length; n > 0; n++) { 
 
-			System.out.println(zahlen[n]); 
+		for (int i : zahlen)
+			System.out.println(i);
+
+		
+		quicksort(0, zahlen.length - 1);
+		for (int i : zahlen)
+			System.out.println(i);
+
 	}
-		for (int links = 0; links < zahlen.length; links++)
-			for (int rechts = n - 1; rechts < zahlen.length; rechts++); 
-	if (links < rechts) {
-		int teiler = teile (links ; rechts)
-				
-			
-	
-	
-	
 
-}
+	static void quicksort(int links, int rechts) {
+
+		if (links < rechts) {
+
+			int teiler = teile(links, rechts);
+
+			quicksort(links, teiler - 1);
+
+			quicksort(teiler + 1, rechts);
+			
+			
+
+		}
+	}
+
+	private static int teile(int links, int rechts) {
+		int i = links;
+		int j = rechts - 1;
+		int pivot = zahlen[rechts];
+
+		do {
+			while (links < rechts - 1 && links < zahlen[rechts]) {
+				i = i + 1;
+			}
+
+			while (rechts - 1 > links && rechts - 1 >= zahlen[rechts]) {
+				j = j - 1;
+
+			}
+
+			if (links > rechts) {
+				int help = zahlen[i];
+				zahlen[i] = zahlen[rechts];
+				zahlen[rechts] = help;
+
+			}
+
+		} while (i < j);
+		if (zahlen[i] > pivot) {
+			int help = zahlen[i];
+			zahlen[i] = zahlen[pivot];
+			zahlen[pivot] = help;
+
+			
+
+		}
+		return i;
+
+	}
+
 }
